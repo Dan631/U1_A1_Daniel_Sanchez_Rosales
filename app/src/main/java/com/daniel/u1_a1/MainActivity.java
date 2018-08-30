1c2b7e1bdcc1bbb2d00ae48507fed6683a55368c
Dan631 <42328545+Dan631@users.noreply.github.com>
2018-08-30 00:31:19 -0500

    Commit

--- app/src/main/java/com/daniel/u1_a1/MainActivity.java
+++ app/src/main/java/com/daniel/u1_a1/MainActivity.java
@@ -10,12 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
         //Inicializar primer textview con Id tv
         TextView tv = findViewById(R.id.tv);
         //Ingresar texto dentro del primer textview
         tv.setText("Daniel Sanchez Rosales");
-
         //Inicializar segundo textview con id alumno
         TextView alumno = findViewById(R.id.alumno);
         //Ingresar texto al segundo textview haciendo llamada al contenido de String.xml
